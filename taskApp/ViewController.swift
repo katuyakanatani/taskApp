37348f55bc1bd499392acacd6534f10cbe3fdfde
katuya.kanatani <bouka.tenken@gmail.com>
2020-01-13 18:50:27 +0900

    Initial Commit

---
+++ taskApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  taskApp
+//
+//  Created by USER on 2020/01/13.
+//  Copyright © 2020 katuya.kanatani. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
